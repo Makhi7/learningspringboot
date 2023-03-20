1346ad5838bef172836370fa1e0187199e3ee778
Makhi7 <mvmangxola22@gmail.com>
2023-03-20 18:12:39 +0200

    finished post mapping and delete mapping in StudentController and StudentService

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,9 +3,14 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository
         <Student,Long>
 {
+    //this method will be transformed to a sql query
+    //@Query("SELECT s FROM Student WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,9 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -19,6 +21,24 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student){
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if(!exists){
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exists");
+        }
+
+        studentRepository.deleteById(studentId);
     }
 }
